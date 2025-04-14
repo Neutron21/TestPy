672b7262afdc59e8e821cfeab5584680499d5d30
victorsilvaHR <victor.hugo.silva01@gmail.com>
2025-04-14 13:03:22 -0600

    se crean get en cotizacion

--- app/routers/cotizacion.py
+++ app/routers/cotizacion.py
@@ -29,6 +29,38 @@ async def get_cotizaciones_by_estatus(estatus: int, session: SessionDep):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No existen Cotizaciones con ese estatus")
     return cotizaciones
 
+@router.get("/cotizacion/nombre/{nombre}", response_model=List[Cotizacion])
+async def get_cotizacion_by_nombre(nombre: str, session: SessionDep):
+    query = select(Cotizacion).where(Cotizacion.nombre == nombre)
+    cotizaciones = session.exec(query).all()
+    if not cotizaciones:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontraron cotizaciones con ese nombre")
+    return cotizaciones
+
+@router.get("/cotizacion/rfc/{rfc}", response_model=List[Cotizacion])
+async def get_cotizacion_by_rfc(rfc: str, session: SessionDep):
+    query = select(Cotizacion).where(Cotizacion.rfc == rfc)
+    cotizaciones = session.exec(query).all()
+    if not cotizaciones:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontraron cotizaciones con ese RFC")
+    return cotizaciones
+
+@router.get("/cotizacion/folio/{folio}", response_model=List[Cotizacion])
+async def get_cotizacion_by_folio(folio: int, session: SessionDep):
+    query = select(Cotizacion).where(Cotizacion.id_cotizacion == folio)
+    cotizaciones = session.exec(query).all()
+    if not cotizaciones:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontraron cotizaciones con ese folio")
+    return cotizaciones
+@router.get("/cotizacion/folio/{folio}", response_model=List[Cotizacion])
+async def get_cotizacion_by_folio(folio: int, session: SessionDep):
+    query = select(Cotizacion).where(Cotizacion.id_cotizacion == folio)
+    cotizaciones = session.exec(query).all()
+    if not cotizaciones:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontraron cotizaciones con ese folio")
+    return cotizaciones
+
+
 @router.post("/cotizacion", response_model=Cotizacion) 
 async def create_cotizacion(usuario_data: CotizacionDTO, session: SessionDep):
     cotizacion = Cotizacion.model_validate(usuario_data.model_dump())  
