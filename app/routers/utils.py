34dcccaab7f6abaa08fc1a87f369721386282313
Pedro Ivan Juárez Cornejo <ivanjuarez@MacBook-Pro-de-Pedro.local>
2025-09-05 16:58:52 -0600

    Ajustando el utils para el borrado de las carpetas

--- app/routers/utils.py
+++ app/routers/utils.py
@@ -31,10 +31,34 @@ def numeros_a_base64(lista_numeros):
 # ---------- ENDPOINTS ----------
 @router.post("/utils", response_model=List[str])
 async def get_base64_from_numeros(payload: NumerosRequest):
+    arrayB64 = numeros_a_base64(payload.numeros)
+    for numB64 in arrayB64:
+        try:
+            # Decodificar base64 → nombre real de la carpeta
+            # folder_name = base64.b64decode(payload.folder_b64).decode("utf-8")
+            folder_name = numB64
+
+            # Obtener ruta desde .env
+            base_path = os.getenv("RUTA_COTIZACIONES")
+            if not base_path:
+                return {"status": "error", "message": "RUTA_COTIZACIONES no está definida en .env"}
+
+            # Construir ruta completa
+            folder_path = os.path.join(base_path, folder_name)
+
+            if os.path.exists(folder_path):
+                shutil.rmtree(folder_path)  # elimina toda la carpeta
+                print({f"OK, Carpeta '{folder_name}' eliminada de cotizaciones"})
+            else:
+                print({f"Error, Carpeta '{folder_name}' no encontrada en cotizaciones"})
+
+        except Exception as e:
+            return {"status": "error", "message": str(e)}
+
     return numeros_a_base64(payload.numeros)
 
 
-@router.delete("/delete-cotizacion")
+# @router.delete("/delete-cotizacion")
 async def delete_cotizacion(payload: DeleteRequest):
     """
     Borra una carpeta dentro de la ruta RUTA_COTIZACIONES,
@@ -43,7 +67,8 @@ async def delete_cotizacion(payload: DeleteRequest):
 
     try:
         # Decodificar base64 → nombre real de la carpeta
-        folder_name = base64.b64decode(payload.folder_b64).decode("utf-8")
+        # folder_name = base64.b64decode(payload.folder_b64).decode("utf-8")
+        folder_name = payload.folder_b64
 
         # Obtener ruta desde .env
         base_path = os.getenv("RUTA_COTIZACIONES")
