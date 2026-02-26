8cd111e79b259ec6929cd7df1bebf09c7520dcac
Pedro Ivan Juárez Cornejo <ivanjuarez@MacBook-Pro-de-Pedro.local>
2026-02-26 15:57:21 -0600

    agregando nuevo enpoint de financieras activas

--- app/routers/financieras.py
+++ app/routers/financieras.py
@@ -18,6 +18,12 @@ async def list_financieras(session: SessionDep):
     financieras = session.exec(query).all()
     return financieras
 
+@router.get("/financieras/activas", response_model=list[Financieras])
+async def list_financieras_activas(session: SessionDep):
+    query = select(Financieras).where(Financieras.fase.in_([1,2])).order_by(Financieras.fase, Financieras.nombre)
+    financieras = session.exec(query).all()
+    return financieras
+
 @router.get("/financieras/tipo/{tipo}", response_model=List[Financieras])
 async def get_financieras_by_tipo(tipo: str, session: SessionDep):
     query = select(Financieras).where(Financieras.tipo == tipo)
