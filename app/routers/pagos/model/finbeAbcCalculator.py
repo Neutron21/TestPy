7fd440b69f1434704788332b51ce38f0d80c5821
Pedro Ivan Juárez Cornejo <ivanjuarez@MacBook-Pro-de-Pedro.local>
2026-04-27 17:31:44 -0600

    ajuste de comisiones del pago al broker

--- app/routers/pagos/model/finbeAbcCalculator.py
+++ app/routers/pagos/model/finbeAbcCalculator.py
@@ -35,16 +35,16 @@ class FinbeAbcCalculator(BaseFinanciera):
         match self.id_membresia:
             case 1:
                 porcentaje_broker = t_pago.c_plata
-                comision_broker = t_pago.c_plata * com_apertura
+                comision_broker = t_pago.c_plata * self.cotizacion.monto
             case 2:
                 porcentaje_broker = t_pago.c_oro
-                comision_broker = t_pago.c_oro * com_apertura
+                comision_broker = t_pago.c_oro * self.cotizacion.monto
             case 3:
                 porcentaje_broker = t_pago.c_platino
-                comision_broker = t_pago.c_platino * com_apertura
+                comision_broker = t_pago.c_platino * self.cotizacion.monto
             case 4:
                 porcentaje_broker = t_pago.c_diamante
-                comision_broker = t_pago.c_diamante * com_apertura
+                comision_broker = t_pago.c_diamante * self.cotizacion.monto
 
         print(f"Membreisa Broker: {MEMBRESIAS.get(self.id_membresia)}")
         print(f"Comision Broker: {comision_broker}")
