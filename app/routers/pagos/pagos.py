ecb4066c329a74b3cf835511a2bbfff597848e36
Pedro Ivan Juárez Cornejo <ivanjuarez@MacBook-Pro-de-Pedro.local>
2026-06-10 17:56:04 -0600

    Ajustando ids de productos de finkargo

--- app/routers/pagos/pagos.py
+++ app/routers/pagos/pagos.py
@@ -22,9 +22,9 @@ async def calular_comisiones_finkargo_incremento(id_cotizacion: int, monto: int,
         monto: Monto sobre el cual se harán los cálculos
         session: Sesión de base de datos
     
-    El producto se usa automáticamente como 172 para Finkargo Incremento de linea.
+    El producto se usa automáticamente como  Finkargo Incremento de linea.
     """
-    return manager_func_finkargo_custom(id_cotizacion, monto, 187, session)
+    return manager_func_finkargo_custom(id_cotizacion, monto, 67, session)
 
 @router.get("/pagos/finkargo-operativa", response_model=ResponsePagos)
 async def calular_comisiones_finkargo_operativa(id_cotizacion: int, monto: int, session: SessionDep):
@@ -36,7 +36,7 @@ async def calular_comisiones_finkargo_operativa(id_cotizacion: int, monto: int, 
         monto: Monto sobre el cual se harán los cálculos
         session: Sesión de base de datos
     
-    El producto se usa automáticamente como 173 para Finkargo Operativa.
+    El producto se usa automáticamente como Finkargo Operativa.
     """ 
    
     return manager_func_finkargo_custom(id_cotizacion, monto, 186, session)
