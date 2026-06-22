b744b9f2abd296f7dd6ca98435d71aa908572417
victorsilvaHR <victor.hugo.silva01@gmail.com>
2026-06-22 10:17:32 -0600

    solucionando problemas

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -61,7 +61,7 @@ async def create_usuario(usuario_data: UsuarioDTO, session: SessionDep):
     # 🔥 CAMBIO IMPORTANTE
     usuario = Usuarios(
         **usuario_dict,
-        comisiones="2026"
+        comisiones="evoluciona"
     )
 
     session.add(usuario)
